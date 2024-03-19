4ca02cfb4f3efda25c058ad4a231dc75efbca9b5
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-03-19 13:33:08 -0300

    Update CreateWorldScreenMixin.java

--- src/main/java/me/duncanruns/beachfilter/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/duncanruns/beachfilter/mixin/CreateWorldScreenMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,7 +19,7 @@ import java.io.IOException;
 
 @Mixin(CreateWorldScreen.class)
 public abstract class CreateWorldScreenMixin extends Screen {
-
+    @Unique
     private boolean doTheThing = false;
 
     @Shadow
